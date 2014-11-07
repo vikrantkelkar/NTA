251933ea08b1d75f1c46f554718ce01eaa682379
vikrantkelkar <vkelkar@gmail.com>
2014-11-06 22:29:13 -0500

    Initial Version

--- ci/application/config/autoload.php
+++ ci/application/config/autoload.php
@@ -37,7 +37,7 @@
 |
 */
 
-$autoload['packages'] = array('database');
+$autoload['packages'] = array();
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array();
 
 
 /*
--- ci/application/views/addStudentForm.php
+++
@@ -1,7 +0,0 @@
-<html>
-	<body>
-		<h1>Add Student</h1>
-		<p><? echo $myValue ?></p>
-		<p><? echo $anotherValue ?></p>
-	</body>
-</html>
